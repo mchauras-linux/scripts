04c3147e9f23582b2238b52a17115fabfd8d211f
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2022-11-13 21:24:58 +0530

    Kill program enhanced for multiple processes
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>

--- mukesh-kill-program.sh
+++ mukesh-kill-program.sh
@@ -5,28 +5,41 @@ then
 	exit -1
 fi
 
-export KILL_PROG_PID=`ps -ax | grep $1 | awk '{print $1}' | head -n 1`
-export KILL_PROG_NAME=`ps -ax | grep $1 | awk '{print $5}' | head -n 1`
-RES="DUMMY"
-RED='\033[0;31m' # RED Color
-NC='\033[0m' # No Color
-while true
-do
-	if [ "$RES" = "Y" ]
-	then
-		kill -9 $KILL_PROG_PID
-		if [ $? -eq 0 ]
+PROG=$1
+
+function kill_program {
+#	KILL_PROG_PID=`ps -ax | grep $PROG | awk '{print $1}' | head -n 1`
+#	KILL_PROG_NAME=`ps -ax | grep $PROG | awk '{$1=$2=$3=$4=""; print $0}' | head -n 1`
+	RES="DUMMY"
+	RED='\033[0;31m' # RED Color
+	NC='\033[0m' # No Color
+	while true
+	do
+		if [ "$RES" = "Y" -o "$RES" = "y" ]
 		then
-			echo "Killed $KILL_PROG_NAME"
+			kill -9 $KILL_PROG_PID
+			if [ $? -eq 0 ]
+			then
+				echo -e "Killed $KILL_PROG_NAME\n"
+			else
+				echo -e "Cannot Kill $KILL_PROG_NAME\n"
+			fi
+			break
+		elif [ "$RES" = "N" -o "$RES" = "n" ]
+		then
+			break
 		else
-			echo "Cannot Kill $KILL_PROG_NAME"
+			echo -e "Do you want to kill ${RED}$KILL_PROG_NAME${NC} (Y/n): "
+			read RES < /dev/tty
 		fi
-		break
-	elif [ "$RES" = "n" ]
-	then
-		break
-	else
-		echo -e "Do you want to kill ${RED}$KILL_PROG_NAME${NC} (Y/n): "
-		read RES
-	fi
-done
+	done
+}
+
+PROCESSES_TO_KILL=$(ps -ax | grep $PROG | head -n-3 )
+
+while IFS= read -r line;
+do
+	KILL_PROG_PID=$(echo $line | awk '{print $1}')
+	KILL_PROG_NAME=$(echo $line | awk '{$1=$2=$3=$4=""; print $0;}')
+	kill_program
+done <<< "$PROCESSES_TO_KILL"
