784b301fa94aa74e25c6becce88bb1e6e8a453d7
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2022-11-19 01:54:50 +0530

    Mail sending added in sync

--- mukesh-sync-ds.sh
+++ mukesh-sync-ds.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
 
-rsync -avh --progress ~/nas/ mchauras@192.168.79.100:/volume1/pub/mukesh/
+# Log file for sync
+LOG_FILE=/tmp/nas_sync.log
+
+# NAS user@ip
+DESTINATION=mchauras@192.168.79.100
+
+# Create Email body
+EMAIL_BODY_FILE=/tmp/mail_body 
+
+echo -e "Data transfer logs to DS\n			\
+\nSource:\n`ifconfig`\n					\
+\nDestination: $DESTINATION" > $EMAIL_BODY_FILE
+
+# Sync with DS and save logs
+rsync -avh --progress ~/nas/ $DESTINATION:/volume1/pub/mukesh/ 2>&1 > $LOG_FILE
+
+# Send email
+cat $EMAIL_BODY_FILE |					\
+mailx -s "sync-ds log"					\
+	-A $LOG_FILE					\
+	mchauras.cron@gmail.com
+
+# Delete Log file
+rm $LOG_FILE
+rm $EMAIL_BODY_FILE
