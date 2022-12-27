a187478e8ef7006f0b5b36dbd414e2a01f3acad8
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2022-12-28 00:32:38 +0530

    Time Lapse script added

---
+++ mukesh-create-time-lapse-from-mp4.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+concat_output=ffmpeg-concatenated.mp4
+output=final.mp4
+list=videos.txt
+
+# remove any prior file input for ffmpeg
+rm $list
+rm $concat_output
+rm $output
+
+# create input file for ffmpeg
+for f in *.mp4
+do
+	echo "file '$f'" >> $list
+done
+
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -f concat -i $list -c copy $concat_output
+
+read -p "Enter framestep for the video [5]: " framestep
+framestep=${framestep:-5}
+
+read -p "Enter FPS for the video [120]: " fps
+fps=${fps:-120}
+
+bitrate=`mediainfo --Output='Video;%BitRate%' $concat_output`
+
+#ffmpeg -an -i $concat_output -vf framestep=$framestep,setpts=N/$fps/TB -r $fps $output
+ffmpeg -hwaccel cuda -hwaccel_output_format cuda -an -i $concat_output -vf framestep=$framestep,setpts=N/$fps/TB -r $fps -c:v h264_nvenc -b:v $bitrate $output
+
+mediainfo $output
