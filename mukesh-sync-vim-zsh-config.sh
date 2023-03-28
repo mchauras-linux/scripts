22c70ce4a5d67c62dd6bcb65fba499bd7fd43795
Mukesh Kumar Chaurasiya <mchauras@linux.vnet.ibm.com>
2023-03-28 11:42:12 +0530

    Updated

--- mukesh-sync-vim-zsh-config.sh
+++ mukesh-sync-vim-zsh-config.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-cp .vimrc scripts/configs 
-cp .zshrc scripts/configs 
-cp .zsh_func scripts/configs 
+cp ~/.vimrc ~/scripts/configs 
+cp ~/.zshrc ~/scripts/configs 
+cp ~/.zsh_func ~/scripts/configs
+
+cd ~/scripts
+
+git add configs/.vimrc configs/.zshrc configs/.zsh_func
+
+git commit -m "Updated vim and zsh config"
+
+git push
