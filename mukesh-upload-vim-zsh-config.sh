1963058b67416786c7607932f48fc2a3828c3c0c
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2023-05-06 22:13:47 +0530

    Updated

--- mukesh-upload-vim-zsh-config.sh
+++ mukesh-upload-vim-zsh-config.sh
@@ -2,12 +2,13 @@
 
 cp ~/.vimrc ~/scripts/configs 
 cp ~/.zshrc ~/scripts/configs 
+cp ~/.zsh_aliases ~/scripts/configs 
 cp ~/.zsh_func ~/scripts/configs
 cp ~/.p10k.zsh ~/scripts/configs
 
 cd ~/scripts
 
-git add configs/.vimrc configs/.zshrc configs/.zsh_func configs/.p10k.zsh
+git add configs/.vimrc configs/.zshrc configs/.zsh_func configs/.p10k.zsh configs/.zsh_aliases
 
 git commit -m "Updated vim and zsh config"
 
