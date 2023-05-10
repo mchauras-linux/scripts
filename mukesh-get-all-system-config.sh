6ef3deb9d004730c3cc270ca654223c9eebc19d1
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-05-10 18:41:24 +0530

    Updated

--- mukesh-get-all-system-config.sh
+++ mukesh-get-all-system-config.sh
@@ -2,13 +2,28 @@
 
 if ! command -v git -v &> /dev/null
 then
+    echo -e "\n"
     echo "git could not be found. Install git to proceed."
     exit
 fi
 
 if ! command -v zsh -v &> /dev/null
 then
+    echo -e "\n"
     echo "zsh could not be found. Install zsh to proceed."
+    exit
+fi
+
+if ! command -v fzf &> /dev/null
+then
+    echo -e "\n"
+    echo "fzf could not be found. Install fzf to proceed."
+    echo "Clone fzf using"
+    echo "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf"
+    echo -e "\n"
+    echo "Install fzf using"
+    echo "~/.fzf/install"
+    echo -e "\n"
     exit
 fi
 
