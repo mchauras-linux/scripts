a2f5786b82ed78dc3b68d1de08428f18c7dd3228
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-05-10 17:43:31 +0530

    Updated

--- mukesh-sync-vim-zsh-config.sh
+++ mukesh-sync-vim-zsh-config.sh
@@ -12,9 +12,16 @@ cp $CONFIG/.zshrc ~/
 cp $CONFIG/.zsh_aliases ~/
 cp $CONFIG/.zsh_func ~/
 cp $CONFIG/.p10k.zsh ~/
-
+cp $CONFIG/fonts/* /usr/share/fonts
 
 if [ ! -d ~/.oh-my-zsh ]
 then
 	git clone git@github.com:ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+	git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
+
+
