7e224f4a174bc97e4c5fdd8ad81350212b52ccb0
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2023-05-14 11:45:07 +0530

    Revert "Updated"
    This reverts commit 8e0b0f2afcceae3e45c115ccc6f13139ad92dcbc.

--- mukesh-sync-system-config.sh
+++ mukesh-sync-system-config.sh
@@ -9,6 +9,9 @@ cd $CONFIG
 git pull
 
 cp $CONFIG/.vimrc ~/ 
+cp $CONFIG/.bashrc ~/ 
+cp $CONFIG/.bash_prompt ~/ 
+cp $CONFIG/.bash_profile ~/ 
 cp $CONFIG/.zshrc ~/
 cp $CONFIG/.zsh_aliases ~/
 cp $CONFIG/.zsh_func ~/
