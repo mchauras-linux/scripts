aa1df18b9978e3d042bc14317f7eb28597665901
Mukesh Kumar Chaursiya <mchauras.linux@gmail.com>
2023-05-14 13:14:46 +0530

    Mukesh pc 2023 05 14 13 05 01 (#3)
    * Updated at 2023-05-14 13:05:01
    
    * Updated
    
    * Updated

--- mukesh-upload-vim-zsh-config.sh
+++ mukesh-upload-vim-zsh-config.sh
@@ -2,23 +2,25 @@
 
 CONFIG=~/scripts/configs
 CONFIG_NEOVIM=~/.config/nvim
+BRANCH="`hostname`-`date "+%Y-%m-%d-%H-%M-%S"`"
 
-cp ~/.vimrc $CONFIG 
-cp ~/.bashrc $CONFIG 
-cp ~/.bash_prompt $CONFIG 
-cp ~/.bash_profile $CONFIG 
-cp ~/.zshrc $CONFIG 
-cp ~/.zsh_func $CONFIG
-cp ~/.zsh_aliases $CONFIG
-cp ~/.p10k.zsh $CONFIG
-cp ~/.tmux.conf $CONFIG 
-cp -rf $CONFIG_NEOVIM $CONFIG
-
-
-cd ~/scripts
-
-if [ -f ~/.zshrc ] 
+if [ -f ~/.mukesh_configured ] 
 then
+	cd ~/scripts
+	
+	git checkout -b $BRANCH
+
+	cp ~/.vimrc $CONFIG 
+	cp ~/.bashrc $CONFIG 
+	cp ~/.bash_prompt $CONFIG 
+	cp ~/.bash_profile $CONFIG 
+	cp ~/.zshrc $CONFIG 
+	cp ~/.zsh_func $CONFIG
+	cp ~/.zsh_aliases $CONFIG
+	cp ~/.p10k.zsh $CONFIG
+	cp ~/.tmux.conf $CONFIG 
+	cp -rf $CONFIG_NEOVIM $CONFIG
+
 	git add 		\
 	configs/.vimrc 		\
 	configs/.bashrc 	\
@@ -30,10 +32,15 @@ then
 	configs/.tmux.conf 	\
 	configs/nvim	 	\
 	configs/.zsh_aliases
-        
+       
+	git add mukesh-*
+
         git status
 
-	git commit -m "Updated vim and zsh config"
+	git commit -m "Updated at `date "+%Y-%m-%d %H:%M:%S"`"
 	echo -e "\n\nPushing All configs to scripts repo\n\n"
-	git push
+	git push --set-upstream origin $BRANCH
 fi
+
+git checkout master
+cd
