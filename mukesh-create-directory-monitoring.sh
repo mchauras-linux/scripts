76d77080da93d2574fad1cf5b0cb859137d90093
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-05-16 22:11:55 +0530

    Directory Monitoring script added

--- mukesh-create-directory-monitoring.sh
+++ mukesh-create-directory-monitoring.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-LOG_FILE="~/monitor_log"
+if [ $# -lt 1 ]
+then
+	echo "Usage:"
+	echo "`basename $0` <directory> <abs(exec)>"
+	echo "Put it in init script for use after reboot"
+	exit 1
+fi
+
+LOG_FILE="/home/$USER/monitor_log"
 
 file_removed() {
-    xmessage "$2 was removed from $1" &
-    $2
+    echo "[$TIMESTAMP]: $2 was removed from $1"  >> $LOG_FILE
+    $3
 }
 
 file_modified() {
     TIMESTAMP=`date`
     echo "[$TIMESTAMP]: The file $1$2 was modified" >> $LOG_FILE
-    $2
+    $3
 }
 
 file_created() {
     TIMESTAMP=`date`
     echo "[$TIMESTAMP]: The file $1$2 was created" >> $LOG_FILE
-    $2
+    $3
 }
 
 inotifywait -q -m -r -e modify,delete,create $1 | while read DIRECTORY EVENT FILE; do
     case $EVENT in
         MODIFY*)
-            file_modified "$DIRECTORY" "$FILE"
+            file_modified "$DIRECTORY" "$FILE" $2
             ;;
         CREATE*)
-            file_created "$DIRECTORY" "$FILE"
+            file_created "$DIRECTORY" "$FILE" $2
             ;;
         DELETE*)
-            file_removed "$DIRECTORY" "$FILE"
+            file_removed "$DIRECTORY" "$FILE" $2
             ;;
     esac
 done
