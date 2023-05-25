a71e96331c04464b6218260c6b8d63ef4b78121c
Mukesh Kumar Chaursiya <mchauras.linux@gmail.com>
2023-05-25 19:20:26 +0530

    Updated at 2023-05-25 19:14:02 (#13)
    Co-authored-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

---
+++ configs/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+-- plugins/telescope.lua:
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.1",
+  -- or                              , branch = '0.1.1',
+  dependencies = { "nvim-lua/plenary.nvim" },
+}
