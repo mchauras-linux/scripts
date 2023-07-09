c2433acc08874ff092715fa6da987f5bcd5edf5e
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-07-09 22:19:28 +0530

    Updated at 2023-07-09 22:19:28

---
+++ configs/nvim/lua/plugins/cscope_maps.lua
@@ -0,0 +1,13 @@
+return {
+  "dhananjaylatkar/cscope_maps.nvim",
+  dependencies = {
+    "folke/which-key.nvim", -- optional [for whichkey hints]
+    "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+    "ibhagwan/fzf-lua", -- optional [for picker="fzf-lua"]
+    "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
+  },
+  opts = {
+    -- USE EMPTY FOR DEFAULT OPTIONS
+    -- DEFAULTS ARE LISTED BELOW
+  },
+}
