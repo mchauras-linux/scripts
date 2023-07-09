b9b460b5da2ecb861f7aa45981f6fa81f27a6b0e
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-07-09 23:37:47 +0530

    Updated at 2023-07-09 23:37:47

--- configs/nvim/lua/plugins/cscope_maps.lua
+++ configs/nvim/lua/plugins/cscope_maps.lua
@@ -7,7 +7,24 @@ return {
     "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
   },
   opts = {
-    -- USE EMPTY FOR DEFAULT OPTIONS
-    -- DEFAULTS ARE LISTED BELOW
+    -- maps related defaults
+    disable_maps = false, -- "true" disables default keymaps
+    skip_input_prompt = false, -- "true" doesn't ask for input
+
+    -- cscope related defaults
+    cscope = {
+      -- location of cscope db file
+      db_file = "./cscope.out",
+      -- cscope executable
+      exec = "cscope", -- "cscope" or "gtags-cscope"
+      -- choose your fav picker
+      picker = "fzf-lua", -- "telescope", "fzf-lua" or "quickfix"
+      -- "true" does not open picker for single result, just JUMP
+      skip_picker_for_single_result = true, -- "false" or "true"
+      -- these args are directly passed to "cscope -f <db_file> <args>"
+      db_build_cmd_args = { "-bqkv" },
+      -- statusline indicator, default is cscope executable
+      statusline_indicator = nil,
+    },
   },
 }
