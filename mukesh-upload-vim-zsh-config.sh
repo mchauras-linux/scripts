8c029dc226cf2eb70ef42f1e75ba0859f0ee53d6
Mukesh Kumar Chaursiya <mchauras.linux@gmail.com>
2023-07-16 12:35:32 +0530

    Updated at 2023-07-16 12:34:44 (#31)

--- mukesh-upload-vim-zsh-config.sh
+++ mukesh-upload-vim-zsh-config.sh
@@ -2,44 +2,43 @@
 
 CONFIG=~/scripts/configs
 CONFIG_NEOVIM=~/.config/nvim
-BRANCH="`hostname`-`date "+%Y-%m-%d-%H-%M-%S"`"
+BRANCH="$(hostname)-$(date "+%Y-%m-%d-%H-%M-%S")"
 
-if [ -f ~/.mukesh_configured ] 
-then
+if [ -f ~/.mukesh_configured ]; then
 	cd ~/scripts
 	git pull
-	cp ~/.vimrc $CONFIG 
-	cp ~/.bashrc $CONFIG 
-	cp ~/.bash_prompt $CONFIG 
-	cp ~/.bash_profile $CONFIG 
-	cp ~/.zshrc $CONFIG 
+	cp ~/.vimrc $CONFIG
+	cp ~/.bashrc $CONFIG
+	cp ~/.bash_prompt $CONFIG
+	cp ~/.bash_profile $CONFIG
+	cp ~/.zshrc $CONFIG
 	cp ~/.zsh_func $CONFIG
 	cp ~/.zsh_aliases $CONFIG
 	cp ~/.p10k.zsh $CONFIG
-	cp ~/.tmux.conf $CONFIG 
+	cp ~/.tmux.conf $CONFIG
 	cp -rf $CONFIG_NEOVIM $CONFIG
 
-	if [[ `git status --porcelain` ]]; 
-	then
+	if [[ $(git status --porcelain) ]]; then
 		git checkout -b $BRANCH
 
-		git add 		\
-		configs/.vimrc 		\
-		configs/.bashrc 	\
-		configs/.bash_prompt 	\
-		configs/.bash_profile 	\
-		configs/.zshrc 		\
-		configs/.zsh_func 	\
-		configs/.p10k.zsh 	\
-		configs/.tmux.conf 	\
-		configs/nvim	 	\
-		configs/.zsh_aliases
-	       
+		git add \
+			configs/.vimrc \
+			configs/.bashrc \
+			configs/.bash_prompt \
+			configs/.bash_profile \
+			configs/.zshrc \
+			configs/.zsh_func \
+			configs/.p10k.zsh \
+			configs/.tmux.conf \
+			configs/nvim \
+			configs/.zsh_aliases
+
 		git add mukesh-*
-	
-	        git status
-	
-		git commit -m "Updated at `date "+%Y-%m-%d %H:%M:%S"`"
+		git add docs
+
+		git status
+
+		git commit -m "Updated at $(date "+%Y-%m-%d %H:%M:%S")"
 		echo -e "\n\nPushing All configs to scripts repo\n\n"
 		git push --set-upstream origin $BRANCH
 	fi
