e07d5324c87f5056365e3fafb933717d245f5c8c
Mukesh Kumar Chaurasiya <mchauras@li-e1dea04c-3555-11b2-a85c-f57333552245.ibm.com>
2023-07-26 13:01:35 +0530

    Dnf repo localization script added
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

---
+++ mukesh-localalize-dnf-repo.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+then 
+	echo "Please run as root"
+	exit -1
+fi
+
+if [ $# -ne 1 ]; 
+then 
+	echo "Illegal number of parameters"
+	echo "Usage: "
+	echo "script <url of repo file>"
+	exit -2
+fi
+
+FILE=`basename $1`
+wget $1
+
+cleanup() {
+	rm -rf /etc/yum.repos.d/$FILE
+	echo "cleanup"
+}
+
+ID=$(head -n 1 $FILE | sed 's/^.//' | sed 's/.$//')
+
+echo $ID
+mv $FILE /etc/yum.repos.d/
+
+dnf reposync --repoid $ID
+
+trap cleanup EXIT
