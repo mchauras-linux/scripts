e61bbb73ed2092f4a78421202ff3279b05bea054
Mukesh Kumar Chaurasiya <mchauras.linux@gmail.com>
2024-07-31 16:56:25 +0530

    Updated at 2024-07-31 16:55:12 (#55)
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
    Co-authored-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- mukesh-prepare-kexec.sh
+++ mukesh-prepare-kexec.sh
@@ -2,12 +2,26 @@
 
 INITRD=/tmp/mukeshinitrd.img
 
-# Check if the argument is --kexec
-if [[ "$1" == "--kexec" ]]; then
-	kexec --initrd=$INITRD ./vmlinux --command-line="$(cat /proc/cmdline)"
-else
+prepare() {
 	make -j$(nproc)
 	make -j$(nproc) modules_install
 	dracut $INITRD --kver=$(make kernelrelease) --force
 	ls $INITRD
+	echo "Rerun with --kexec option to perform kexec"
+}
+
+kexec() {
+	if test -f $INITRD; then
+		kexec --initrd=$INITRD ./vmlinux --command-line="$(cat /proc/cmdline)"
+	else
+		echo "Running prerequisites...!!!"
+		prepare
+	fi
+}
+
+# Check if the argument is --kexec
+if [[ "$1" == "--kexec" ]]; then
+	kexec
 fi
+	
+prepare
---
+++ mukesh-skiboot-setup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+export BASE_DIR=/home/mchauras/src
+export SKIBOOT_DIR=$BASE_DIR/skiboot
+export SKIBOOT=$SKIBOOT_DIR/skiboot.lid
+export SKIBOOT_ZIMAGE=$BASE_DIR/linux-ppc/vmlinux
+export SKIBOOT_INITRD=
+
