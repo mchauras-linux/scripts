08aa199349a87457b64fc9f2ebaa4690418213c3
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-08-27 01:00:00 +0530

    Auto Updating Agenda
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- agenda/mukesh-send-agenda.sh
+++ agenda/mukesh-send-agenda.sh
@@ -5,10 +5,10 @@ subject="Agenda for the week"
 recipient="mchauras@hotmail.com"
 cc_recipients="-c mukeshmike9@gmail.com -c mukesh.chaurasiya@ibm.com"
 file_to_send="/tmp/agenda.txt"
-body="Agenda for this week is:
+body="Agenda for the month is:
 `cat $file_to_send`
 "
 # Use mutt to send the email
-#echo "$body" | /usr/bin/mailx -s "$subject" $cc_recipients -a $file_to_send $recipient
-echo "$body" | /usr/bin/mailx -s "$subject" $cc_recipients $recipient
+#echo "$body" | /usr/bin/mailx -s "$subject" $cc_recipients -a $file_to_send -C "Content-Type: text/html" $recipient
+echo "$body" | /usr/bin/mailx -s "$subject" $cc_recipients -C "Content-Type: text/html" $recipient
 
