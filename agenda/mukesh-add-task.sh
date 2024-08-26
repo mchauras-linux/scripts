519856fef09c97bc2669ac4a7ae922e618b3cd50
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-08-26 06:51:00 +0530

    Auto Updating Agenda
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- agenda/mukesh-add-task.sh
+++ agenda/mukesh-add-task.sh
@@ -8,25 +8,13 @@ fi
 
 # Path to the Org file
 ORG_FILE="$HOME/scripts/agenda/agenda.org"
-cat $ORG_FILE
+
 # Combine all command-line arguments into a single task description
 TASK_DESCRIPTION="$*"
 
 # Get the next day's date in Org mode format
 NEXT_DAY=$(date -d "tomorrow" +"%Y-%m-%d")
 
-# Emacs Lisp code to add the task
-EMACS_LISP_CODE=$(cat <<EOF
-(require 'org)
-(find-file "$ORG_FILE")
-(goto-char (point-max))
-(insert "** TODO $TASK_DESCRIPTION\n")
-(insert "  DEADLINE: <${NEXT_DAY}>")
-(save-buffer)
-(kill-emacs)
-EOF
-)
-
-# Run Emacs in batch mode with the Elisp code
-emacs -batch -l ~/.spacemacs -e "$EMACS_LISP_CODE"
+echo -e "** TODO $TASK_DESCRIPTION\n" >> $ORG_FILE
+echo -e "  DEADLINE: <${NEXT_DAY}>\n" >> $ORG_FILE
 
