bf12eff35dede55735df811387ddecf636176877
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-08-27 01:22:00 +0530

    Auto Updating Agenda
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- agenda/mukesh-export-agenda.el
+++ agenda/mukesh-export-agenda.el
@@ -8,8 +8,9 @@
 (setq org-agenda-span 'month)
 
 ;; Generate the agenda and export it to a file
-(let ((output-file "/tmp/agenda.txt"))  ; Change this to your desired output file
+(let ((output-file "/tmp/agenda.html"))  ; Change this to your desired output file
   (with-temp-buffer
     (org-agenda-list)
     (org-batch-agenda "a")
+    (org-agenda-write output-file)
     (write-file output-file)))
