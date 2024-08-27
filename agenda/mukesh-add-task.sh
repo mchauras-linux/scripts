f5bf6c88f4821989ed8f22b434c8eef1f2a7dc1e
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-08-27 17:27:45 +0530

    Manual update
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- agenda/mukesh-add-task.sh
+++ agenda/mukesh-add-task.sh
@@ -2,16 +2,17 @@
 
 # Function to display usage
 show_help() {
-    echo "Usage: addtask <deadline> <task>"
-    echo "  <deadline>      Number of days until the deadline"
-    echo "                  must be a positive integer(0 for Today)"
-    echo "  <task>          Description of the task"
-    exit 1
+	echo "Usage: addtask <deadline> <task>"
+	echo "  <deadline>      Number of days until the deadline"
+	echo "                  must be a positive integer(0 for Today)"
+	echo "                  OR DDD format e.g. Mon, Tue etc"
+	echo "  <task>          Description of the task"
+	exit 1
 }
 
 # Check if two arguments are passed
 if [ "$#" -ne 2 ]; then
-    show_help
+	show_help
 fi
 
 # Assign arguments to variables
@@ -20,12 +21,20 @@ task="$2"
 
 # Check if deadline_day is a valid number
 if ! [[ "$deadline_day" =~ ^[0-9]+$ ]]; then
-    echo -e "Error: <deadline> must be a positive integer\n"
-    show_help
+
+	# Try to calculate the next occurrence of the specified day
+	deadline_date=$(date -d "next $deadline_day" +"%Y-%m-%d %a" 2>/dev/null)
+
+	# Check if the date command was successful
+	if ! [ $? -eq 0 ]; then
+		echo -e "Error: Incorrect Deadline\n"
+		show_help
+	fi
+else
+	# Get the future date based on the deadline day
+	deadline_date=$(date -d "+$deadline_day days" +"%Y-%m-%d %a")
 fi
 
-# Get the future date based on the deadline day
-deadline_date=$(date -d "+$deadline_day days" +"%Y-%m-%d %a")
 
 # Print the outputdd
 echo "$task: $deadline_date"
