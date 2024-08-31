630e89f06722be8acccfa13723bac8b6ccceddf6
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2024-09-01 02:04:41 +0530

    Auto Updating Agenda
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- agenda/mukesh-get-agenda.sh
+++ agenda/mukesh-get-agenda.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+AGENDA_FILE="~/.agenda.html"
+TEMP_AGENDA_FILE="~/.agenda_temp.html"
+
 cd ~/scripts
 
 if [[ $(git status agenda --porcelain) ]]; then
@@ -9,16 +12,16 @@ if [[ $(git status agenda --porcelain) ]]; then
 	git push
 fi
 
-mv /tmp/agenda.html /tmp/prev_agenda.html
+mv $AGENDA_FILE $TEMP_AGENDA_FILE
 #/usr/bin/emacs -batch -l ~/scripts/agenda/mukesh-export-agenda.el
 emacs --eval '(progn
                  (setq org-agenda-files (list "~/scripts/agenda/agenda.org"))              
                  (org-agenda-list)
-                 (org-agenda-write (expand-file-name "/tmp/agenda.html"))
+                 (org-agenda-write (expand-file-name "~/.agenda.html"))
                  (save-buffers-kill-emacs))'
 
-if ! cmp -s /tmp/agenda.html /tmp/prev_agenda.html; then
+if ! cmp -s $AGENDA_FILE $TEMP_AGENDA_FILE; then
 	# Send email
 	echo "Agenda has changed"
-	~/scripts/agenda/mukesh-send-agenda.sh
+	#~/scripts/agenda/mukesh-send-agenda.sh
 fi
